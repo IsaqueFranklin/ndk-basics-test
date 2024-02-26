5001d2cfff48c0ef6b424f7dabc51ad0dda45ea7
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-02-26 20:01:32 -0300

    design modifics

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -12,19 +12,20 @@ export default function Event({ event }) {
       if (event.kind === 0) {
         const parsed = JSON.parse(event.content);
         return (
-          <div>
-            <h3>{parsed.name}</h3>
-            <img src={parsed.picture} width={100} height={100} alt="" />
-            <div>{parsed.about}</div>
+          <div className="p-6 flex border border-white rounded-2xl max-w-4xl">
+            <img className="rounded-full h-32 w-32" src={parsed.picture} alt="" />
+            <div className="mx-4 my-auto">
+                <h3 className="text-3xl font-semibold mb-2">{parsed.name}</h3>
+                <p>{parsed.about}</p>
+            </div>
           </div>
         );
       } else {
         // TODO: render other kinds of events. Start with kind 1 (plain text note)
         return (
-          <div>
+          <div className="max-w-4xl">
             <pre
               style={{
-                background: "#f4f4f4",
                 padding: "10px",
                 borderRadius: "5px",
                 fontFamily: "monospace",
