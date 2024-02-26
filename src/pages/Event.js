54867ae702a0d060c714b906cb0f51d4ebf64223
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-02-26 19:40:42 -0300

    initial commit

---
+++ src/pages/Event.js
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Event({ event }) {
+    const stringify = (event) => {
+      return JSON.stringify(
+        event.rawEvent(),
+        null,
+        2
+      );
+    };
+  
+    const renderEvent = (event) => {
+      if (event.kind === 0) {
+        const parsed = JSON.parse(event.content);
+        return (
+          <div>
+            <h3>{parsed.name}</h3>
+            <img src={parsed.picture} width={100} height={100} alt="" />
+            <div>{parsed.about}</div>
+          </div>
+        );
+      } else {
+        // TODO: render other kinds of events. Start with kind 1 (plain text note)
+        return (
+          <div>
+            <pre
+              style={{
+                background: "#f4f4f4",
+                padding: "10px",
+                borderRadius: "5px",
+                fontFamily: "monospace",
+              }}
+            >
+              {stringify(event)}
+            </pre>
+          </div>
+        );
+      }
+    };
+  
+    return <div>{event && renderEvent(event)}</div>;
+  }
---
+++ src/pages/Search.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import styles from "@/styles/Home.module.css";
+import { ndk } from "@/pages/index";
+import { nip19 } from "nostr-tools";
+import Event from "./Event";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+  const [ndkEvent0, setNdkEvent0] = useState(null);
+  const [ndkEvent1, setNdkEvent1] = useState(null);
+
+  // to serach for a kind 0 event singed by a specific pubkey
+  const kind0Filter = (pubkey) => ({
+    kinds: [0],
+    authors: [pubkey],
+  });
+
+  const kind1Filter = (pubkey) => ({
+    kinds: [1],
+    authors: [pubkey],
+  });
+
+  // to search for an event by it's id
+  const eventIdFilter = (id) => ({
+    ids: [id],
+  });
+
+  // takes a query in the form of "note..." or "npub..."
+  // TODO: expand to accept more query types
+  const search = async (query) => {
+    // tell the relays what we're looking for with a "filter"
+    // https://github.com/nostr-protocol/nips/blob/master/01.md#communication-between-clients-and-relays
+    let filter0 = {};
+    let filter1 = {};
+
+    // nip19 defines a standard for bech32 encoding of different data types (pubkey, note id, etc.)
+    // https://github.com/nostr-protocol/nips/blob/master/19.md
+    if (query.startsWith("npub")) {
+      const decodedNpub = nip19.decode(query);
+      const pubkey = decodedNpub.data;
+
+      filter0 = kind0Filter(pubkey); // TODO: search for profile (kind 0) along with kind 1 events authored by this pubkey
+      filter1 = kind1Filter(pubkey);
+    } else if (query.startsWith("note")) {
+      const decodedNote = nip19.decode(query);
+      const noteId = decodedNote.data;
+
+      filter1 = eventIdFilter(noteId);
+    }
+
+    //console.log("SEARCH FILTER", filter);
+
+    // fetchEvent takes a type of NDKFilter
+    // see node_modules/@nostr-dev-kit/ndk/dist/index.d.ts --> "type NDKFilter"
+    return await ndk.fetchEvent(filter0) && await ndk.fetchEvents(filter1);
+  };
+
+  const handleSearch = async (query) => {
+    const result = await search(query);
+    if (result) {
+      console.log("SEARCH RESULT", result);
+      setNdkEvent0(result);
+    }
+  };
+
+  return (
+    <div className={`${styles.search}`}>
+      <h2>Search</h2>
+      <p>Search for a profile by npub... or a note by note...</p>
+      <div className={`${styles.searchBar}`}>
+        <input
+            className="bg-black text-white border rounded-2xl border-white px-4 py-2"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => handleSearch(input)} className="bg-white text-black rounded-2xl">Search</button>
+      </div>
+      {/*<Event event={ndkEvent} />*/}
+      <hr />
+    </div>
+  );
+};
+
+export default Search;
